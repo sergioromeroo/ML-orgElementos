9a15f14e53387c58d3207f9d1defc051c251cb90
sergioromeroo <manusergio96@gmail.com>
2021-11-18 13:30:04 -0300

    agrego org elemento

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require ('express');
+const app = express();
+const path = require('path');
+const port = 3030;
+
+app.listen(port, () => console.log('Servidor corriendo en el puerto ' + port));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'views', 'home.html')));
+app.get('/register', (req, res) => res.sendFile(path.join(__dirname, 'views', 'register.html')));
+app.get('/login', (req, res) => res.sendFile(path.join(__dirname, 'views', 'login.html')));
+app.use(express.static('public'));
